435500e640ceea985f2bbd3cb808b752bb02b2ee
Aliaksei Karzhou <alexius90e@gmail.com>
2024-02-19 20:02:39 +0300

    feat: added contacts modal

--- js/modals.js
+++ js/modals.js
@@ -4,15 +4,33 @@ modals.forEach((modal) => {
   modal.addEventListener("click", (event) => {
     const isModal = event.target === event.currentTarget;
     const isCloseButton = event.target.classList.contains("modal__close");
-    if (isModal || isCloseButton) callbackModal.classList.remove("active");
+    if (isModal || isCloseButton) event.currentTarget.classList.remove("active");
   });
 });
 
 const callbackModalButtons = document.querySelectorAll(".button_callback");
-const callbackModal = document.querySelector(".modal-callback");
 
 callbackModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
+    const callbackModal = document.querySelector(".modal-callback");
     if (callbackModal) callbackModal.classList.add("active");
+  });
+});
+
+const aboutModalButtons = document.querySelectorAll(".button_about");
+
+aboutModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const aboutModal = document.querySelector(".modal-about");
+    if (aboutModal) aboutModal.classList.add("active");
+  });
+});
+
+const contactsModalButtons = document.querySelectorAll(".button_contacts");
+
+contactsModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const contactsModal = document.querySelector(".modal-contacts");
+    if (contactsModal) contactsModal.classList.add("active");
   });
 });
